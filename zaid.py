654de82479e2cb92b39bc9dcb7a1a39c83a6f3bb
Zaid khan <zaidkhanreal7863@gmail.com>
2025-11-17 10:03:59 -0800

    Add files via upload

---
+++ zaid.py
@@ -0,0 +1,22 @@
+
+age=23
+price=68.4
+print ('my age is:', age)
+#assigment operator
+num=10
+num **=6
+print('num:',num) 
+
+#logical operator
+print(not False)
+print(not False)
+val= True
+val1=False
+print("and operator:", val and val1)
+a="6" 
+c=int(a) #type castimg or coversion
+b=2.3
+sum=b+c
+print(sum)
+name = input("entr your name;")
+print("welcome",name)
